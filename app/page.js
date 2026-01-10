6a84e1bc2a67a6c024f994cb16a0eacec0713f9f
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-01-10 19:47:36 +0530

    Add home page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>RC Mentor AI</h1>
+      <p>This is the beginning.</p>
+    </main>
+  );
+}
