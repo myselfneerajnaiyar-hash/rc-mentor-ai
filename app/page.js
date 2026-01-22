f19043b12659055ae2d46e50f2df070ad9052dc4
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-01-23 01:40:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1438,7 +1438,7 @@ export default function Page() {
                   dayBlock(
                     i,
                     `${weakestType.toUpperCase()} Mastery`,
-                    `Solve 3 RCs focusing only on ${weakestType} questions. After each paragraph, summarize in one line`.
+                    `Solve 3 RCs focusing only on ${weakestType} questions. After each paragraph, summarize in one line.`
                   )
                 );
               } else if (i <= 10) {
