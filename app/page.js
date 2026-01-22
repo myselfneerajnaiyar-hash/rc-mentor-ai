cbb79995b3208ee45e4bfd3c6380344fa4e256a3
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-01-22 18:49:40 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1503,6 +1503,7 @@ const showGenPanel = showGenerator && !isAdaptive;
 )}
     </div>
   </div>
+)}
   {phase === "loading-adaptive" && (
   <div style={{ padding: 40, textAlign: "center", fontSize: 18 }}>
     Preparing your next adaptive passage…
