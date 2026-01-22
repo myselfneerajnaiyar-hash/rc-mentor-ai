8ab683ca52bfb8ac37636c524108d35448d695f0
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-01-22 15:58:20 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1467,7 +1467,8 @@ const showGenPanel = showGenerator && !isAdaptive;
       <p><b>Root:</b> {w.root}</p>
     )}
   </div>
-)}
+);
+   })()}
      <button
   style={{ marginTop: 16 }}
   onClick={() => {
