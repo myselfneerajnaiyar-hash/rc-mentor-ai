63456d92b85f7be6de8a9fc94e44306bf5c8e1cc
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-01-28 18:19:52 +0530

    Update RCView.jsx

--- components/RCView.jsx
+++ components/RCView.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import MentorView from "./MentorView";
 import Navbar from "./Navbar";
 import RCProfile from "./RCProfile";
-import SpeedDashboard from "./SpeedDashboard";
 
 export default function RCView({view,setView }) {
   const [text, setText] = useState("");
