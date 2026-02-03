69c2b656c5fedc2f6e078b69756814298fd4a81c
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-02-04 00:06:20 +0530

    Create sectionals.js

---
+++ app/api/cat-sectionals/sectionals.js
@@ -0,0 +1,7 @@
+import sectional01 from "@/data/catrc/sectional-01.json";
+import sectional02 from "@/data/catrc/sectional-02.json";
+
+export const CAT_SECTIONALS = {
+  "sectional-01": sectional01,
+  "sectional-02": sectional02,
+};
