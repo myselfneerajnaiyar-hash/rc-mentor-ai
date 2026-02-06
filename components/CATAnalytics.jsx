cfd63ec18d60c4cfd2ab3073e77fa7e837743a38
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-02-06 09:03:44 +0530

    Update CATAnalytics.jsx

--- components/CATAnalytics.jsx
+++ components/CATAnalytics.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 const STORAGE_KEY = "catRCResults";
 const [compareA, setCompareA] = useState("");
 const [compareB, setCompareB] = useState("");
