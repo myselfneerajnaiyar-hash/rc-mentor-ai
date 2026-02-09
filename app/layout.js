2eddeb795ce87e5a3bdcb892c2a83acb78fd9995
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-02-10 02:38:20 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import InstallAppButton from "./components/InstallAppButton";
 
 export const metadata = {
   title: "AuctorRC",
@@ -9,29 +10,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
-
-        {/* Theme color for Android status bar */}
         <meta name="theme-color" content="#2563eb" />
-
-        {/* Mobile viewport */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
       </head>
       <body>
         {children}
-            <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js');
-      }
-    `,
-  }}
-/>
+        <InstallAppButton />
       </body>
     </html>
   );
