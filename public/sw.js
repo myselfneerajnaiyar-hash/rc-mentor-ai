e1d7c49eccc95944d176be63e9af0ac10569952d
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-02-09 23:26:19 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,15 @@
-const SW_VERSION = "auctorrc-sw-v1";
+const SW_VERSION = "rc-mentor-sw-v3"; // 🔥 CHANGE THIS EVERY DEPLOY
 
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
+  console.log("SW installing:", SW_VERSION);
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
+  console.log("SW activating:", SW_VERSION);
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
     )
   );
   self.clients.claim();
