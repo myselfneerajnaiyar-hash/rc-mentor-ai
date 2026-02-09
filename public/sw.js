a28fe1667d6f3176a6d1630beecf76cd74e82355
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2026-02-09 23:13:28 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,18 @@
-self.addEventListener("install", (event) => {
+const SW_VERSION = "auctorrc-sw-v1";
+
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
