cc62c56f376c10c77201fd7a9fef3abc245fe9ab
Neraj Naiyar <myselfneerajnaiyar@gmail.com>
2026-06-11 12:16:41 +0530

    Disable caching for rc leaderboard

--- app/api/rc-leaderboard/route.js
+++ app/api/rc-leaderboard/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server"
 import { createClient } from "@supabase/supabase-js"
-
+export const dynamic = "force-dynamic"
+export const revalidate = 0
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
