acf49b8bec5bf9253404918355547c977b16b4dd
Neraj Naiyar <myselfneerajnaiyar@gmail.com>
2026-07-03 15:08:48 +0530

    Add Meta Pixel

--- app/layout.js
+++ app/layout.js
@@ -33,9 +33,33 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-18259887177');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1724851152297636');
+    fbq('track', 'PageView');
+  `}
+</Script>
 
         <PostHogProvider />
         {children}
+        <noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=1724851152297636&ev=PageView&noscript=1"
+    alt=""
+  />
+</noscript>
       </body>
     </html>
   );
