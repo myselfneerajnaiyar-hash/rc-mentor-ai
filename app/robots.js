a3fe7407d269477696fffe4eb22d9dda57de0e44
Neraj Naiyar <myselfneerajnaiyar@gmail.com>
2026-07-06 13:33:52 +0530

    Update sitemap

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://rc.auctorlabs.in/sitemap.xml",
+  };
+}
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,45 @@
 export default function sitemap() {
+  const baseUrl = "https://rc.auctorlabs.in";
+
   return [
     {
-      url: "https://rc.auctorlabs.in",
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/preview`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/pricing`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/signup`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/workout`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/welcome`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/daily-challenge`,
       lastModified: new Date(),
     },
   ];
