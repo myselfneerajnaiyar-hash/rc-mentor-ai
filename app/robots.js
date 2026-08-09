3a9f0384df0be346543a256b3387d5723e509e33
Neraj Naiyar <myselfneerajnaiyar@gmail.com>
2026-08-09 23:19:31 +0530

    Add robots.txt for Auctor RC

--- app/robots.js
+++ app/robots.js
@@ -2,8 +2,7 @@ export default function robots() {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      disallow: "/",
     },
-    sitemap: "https://rc.auctorlabs.in/sitemap.xml",
   };
 }
